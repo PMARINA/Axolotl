8f08e4bd31b0583210115759dfa51ea4d88adc9f
axolotlcreator <mndodds47@gmail.com>
2017-01-19 16:54:03 -0500

    Update sketch_jan13a.ino

--- sketch_jan13a/sketch_jan13a.ino
+++ sketch_jan13a/sketch_jan13a.ino
@@ -1,12 +1,24 @@
 #include <Servo.h>;
+#include <SharpIR.h>;
 Servo left;
 Servo right;
+SharpIR leftSensor(A5, 25, 95, 20150);
+SharpIR frontSensor(A4, 25, 95, 20150);
+
 void setup() {
   left.attach(9);
   right.attach(10);
+  Serial.begin(9600);
 }
 
 void loop() {
-  left.write(122);
-  right.write(0);
+  //left.write(110);
+  //right.write(0);
+  left.write(90);
+  right.write(90);
+  Serial.print("Left: ");
+  Serial.print(leftSensor.distance());
+  Serial.print("    Front: ");
+  Serial.println(frontSensor.distance());
+  delay(500);
 }
